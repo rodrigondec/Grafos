67936bea1e812e66b0a1561b3ae50dc1500ff603
RodrigoNdeC <rodrigondec@gmail.com>
2016-08-09 22:43:59 -0300

    removido count

--- Aresta.py
+++ Aresta.py
@@ -1,12 +1,9 @@
 class Aresta(object):
 	"""docstring for Aresta"""
-	count = 0
-
 	def __init__(self, identificador1, identificador2):
 		super(Aresta, self).__init__()
 		self.identificador1 = identificador1
 		self.identificador2 = identificador2
-		Aresta.count += 1
 
 	def __str__(self):
 		return '['+self.identificador1.__str__()+', '+self.identificador2.__str__()+']'
