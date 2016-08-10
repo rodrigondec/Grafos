67270e38b601a780c30008bbf63adf926f8428eb
RodrigoNdeC <rodrigondec@gmail.com>
2016-08-09 21:25:02 -0300

    classe dos nós finalizada

---
+++ No.py
@@ -0,0 +1,19 @@
+class No(object):
+	"""docstring for ClassName"""
+	count = 0
+	def __init__(self, identificador):
+		super(No, self).__init__()
+		self.identificador = identificador
+		No.count += 1
+
+	def __str__(self):
+		return '['+self.identificador.__str__()+']'
+		
+		
+class NoValor(No):
+	"""docstring for NoValued"""
+	def __init__(self, identificador, valor):
+		No.__init__(self, identificador)
+		self.valor = valor
+	def __str__(self):
+		return '['+self.identificador.__str__()+'] {'+self.valor.__str__()+'}'
