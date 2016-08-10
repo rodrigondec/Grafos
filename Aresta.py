dca6ddc08dbd25045a4dcf54a68d221d30bd3794
RodrigoNdeC <rodrigondec@gmail.com>
2016-08-09 22:57:46 -0300

    minor fix

--- Aresta.py
+++ Aresta.py
@@ -11,11 +11,14 @@ class Aresta(object):
 	def str(self):
 		return '['+self.identificador1.__str__()+', '+self.identificador2.__str__()+']'
 
-class ArestaValor(Aresta):
+class ArestaValorada(Aresta):
 	"""docstring for ArestaValor"""
 	def __init__(self, identificador1, identificador2, valor):
 		Aresta.__init__(self, identificador1, identificador2)
 		self.valor = valor
 	
 	def __str__(self):
+		return '['+self.identificador1.__str__()+', '+self.identificador2.__str__()+'] {'+self.valor.__str__()+'}'
+
+	def str(self):
 		return '['+self.identificador1.__str__()+', '+self.identificador2.__str__()+'] {'+self.valor.__str__()+'}'
