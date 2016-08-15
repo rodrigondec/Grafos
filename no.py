2094e0e7b8ad0105ea4040a14fe58865f526a655
RodrigoNdeC <rodrigondec@gmail.com>
2016-08-15 18:16:49 -0300

    adicionado noarvoredist

--- no.py
+++ no.py
@@ -23,11 +23,21 @@ class NoValorado(No):
 	def str(self):
 		return '['+self.identificador.__str__()+'] {'+self.valor.__str__()+'}'
 
-
 class NoArvore(No):
-	def __init__(self, identificador, pai, distancia):
+	def __init__(self, identificador, pai):
 		No.__init__(self, identificador)
 		self.pai = pai
+
+	def __str__(self):
+		return '['+self.identificador.__str__()+'] {'+self.pai.__str__()+'}'
+
+	def str(self):
+		return '['+self.identificador.__str__()+'] {'+self.pai.__str__()+'}'
+
+
+class NoArvoreDist(NoArvore):
+	def __init__(self, identificador, pai, distancia):
+		NoArvore.__init__(self, identificador, pai)
 		self.distancia = distancia
 
 	def __str__(self):
