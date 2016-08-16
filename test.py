30d5121250037dba3674b6fbf6fb2f55897c170a
RodrigoNdeC <rodrigondec@gmail.com>
2016-08-15 22:48:03 -0300

    teste do dfs

--- test.py
+++ test.py
@@ -27,7 +27,29 @@ class TestStringMethods(unittest.TestCase):
 
 		self.assertEqual(len(self.grafo.arestas), 7)
 
-		print self.grafo.bfs(1)
+		self.grafo.bfs(1)
+
+	def test_dfs(self):
+		self.grafo.insertNo(No(1))
+		self.grafo.insertNo(No(2))
+		self.grafo.insertNo(No(3))
+		self.grafo.insertNo(No(4))
+		self.grafo.insertNo(No(5))
+		self.grafo.insertNo(No(6))
+
+		self.assertEqual(len(self.grafo.nos), 6)
+
+		self.grafo.insertAresta(Aresta(1, 2))
+		self.grafo.insertAresta(Aresta(1, 5))
+		self.grafo.insertAresta(Aresta(5, 2))
+		self.grafo.insertAresta(Aresta(5, 4))
+		self.grafo.insertAresta(Aresta(2, 3))
+		self.grafo.insertAresta(Aresta(3, 4))
+		self.grafo.insertAresta(Aresta(4, 6))
+
+		self.assertEqual(len(self.grafo.arestas), 7)
+
+		self.grafo.dfs(1)
 
 if __name__ == '__main__':
 	unittest.main()
