027f946f81c3aebf1ce191d0e3f0ec31862b04ae
RodrigoNdeC <rodrigondec@gmail.com>
2016-08-15 22:57:55 -0300

    adicionado teste para método atingivel()

--- test.py
+++ test.py
@@ -7,6 +7,29 @@ class TestStringMethods(unittest.TestCase):
 	def setUp(self):
 		self.grafo = Grafo()
 
+	def test_atingivel(self):
+		self.grafo.insertNo(No(1))
+		self.grafo.insertNo(No(2))
+		self.grafo.insertNo(No(3))
+		self.grafo.insertNo(No(4))
+		self.grafo.insertNo(No(5))
+		self.grafo.insertNo(No(6))
+		self.grafo.insertNo(No(7))
+		self.assertEqual(len(self.grafo.nos), 7)
+
+		self.grafo.insertAresta(Aresta(1, 2))
+		self.grafo.insertAresta(Aresta(1, 5))
+		self.grafo.insertAresta(Aresta(5, 2))
+		self.grafo.insertAresta(Aresta(5, 4))
+		self.grafo.insertAresta(Aresta(2, 3))
+		self.grafo.insertAresta(Aresta(3, 4))
+		self.grafo.insertAresta(Aresta(4, 6))
+		self.assertEqual(len(self.grafo.arestas), 7)
+
+		self.assertEqual(self.grafo.atingivel(1, 6), True)
+
+		self.assertEqual(self.grafo.atingivel(1, 7), False)
+
 	def test_caminho(self):
 		self.grafo.insertNo(No(1))
 		self.grafo.insertNo(No(2))
